f3c9214c028060fe49b04c97578c7a6a0b77ee5a
Rojan Maharjan <rojanmhj@gmail.com>
2023-03-03 09:42:20 -0800

    Created Models for database UserModel and ProductModel

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,13 +9,13 @@ const PORT = 3000;
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_USER = process.env.MONGO_USER || 'user';
 const MONGO_PASS = process.env.MONGO_PASS || 'pass';
-const mongoURI = `mongodb://mongodb:${MONGO_PORT}`;
+const mongoURI = `mongodb://localhost:${MONGO_PORT}`;
 const mongoOptions = {
     user: MONGO_USER,
     pass: MONGO_PASS
 };
 
-mongoose.connect(mongoURI,mongoOptions)
+mongoose.connect(mongoURI)
 .then(()=> console.log(`Connected to database. port ${MONGO_PORT} user=${MONGO_USER}`))
 .catch(err=>console.log(`Unable to Connect to database \n:${err}`))
 
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}!`);
 });
