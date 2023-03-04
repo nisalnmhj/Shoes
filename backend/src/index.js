33f1292f67f98da678bffbc1a235f771fcd2d7c7
Rojan Maharjan <rojanmhj@gmail.com>
2023-03-04 00:21:21 -0800

    things to do need to do Routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,21 +9,35 @@ const PORT = 3000;
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_USER = process.env.MONGO_USER || 'user';
 const MONGO_PASS = process.env.MONGO_PASS || 'pass';
-const mongoURI = `mongodb://localhost:${MONGO_PORT}`;
+const mongoURI = `mongodb://localhost:${MONGO_PORT}/collection`;
 const mongoOptions = {
     user: MONGO_USER,
     pass: MONGO_PASS
 };
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=> console.log(`Connected to database. port ${MONGO_PORT} user=${MONGO_USER}`))
-.catch(err=>console.log(`Unable to Connect to database \n:${err}`))
+.catch(err=>console.log(`Unable to Connect to database \n:${err}`));
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
+/**
+ * Routes we need
+ * POST: /register
+ * POST: /login
+ * GET: /products
+ * GET: /product/{id}
+ * POST: /product
+ * DELETE: /product
+ * POST: /purchase
+ * 
+ */
+
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}!`);
