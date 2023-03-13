5d587272e5c542fb6484cdc94c1768db74bef59e
Rojan Maharjan <rojanmhj@gmail.com>
2023-03-13 12:09:02 -0700

    Bug Fixed: Moongoose Connection With authentication

--- backend/src/connection/mongooseConnection.js
+++ backend/src/connection/mongooseConnection.js
@@ -6,13 +6,15 @@ function connectDatabase(){
     const MONGO_PORT = process.env.MONGO_PORT || 27017;
     const MONGO_USER = process.env.MONGO_USER || 'user';
     const MONGO_PASS = process.env.MONGO_PASS || 'pass';
-    const mongoURI = `mongodb://mongodb:${MONGO_PORT}/collection`;
+    const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASS}@mongodb:${MONGO_PORT}?authSource=admin`;
     const mongoOptions = {
-    user: MONGO_USER,
-    pass: MONGO_PASS
+    // user: MONGO_USER,
+    // pass: MONGO_PASS,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
             };
 
-    mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology:true})
+    mongoose.connect(mongoURI,mongoOptions)
     .then(()=> console.log(`Connected to database. port ${MONGO_PORT} user=${MONGO_USER}`))
     .catch((e)=>{
         console.log(e);
